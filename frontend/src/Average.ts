affb6cb7cb3c0b4fb4ae1bd3133d9436903c69fd
tona0516 <tonango.0516@gmail.com>
2023-05-02 11:33:22 +0900

    Fix calculate avg logic

--- frontend/src/Average.ts
+++ frontend/src/Average.ts
@@ -1,6 +1,16 @@
 import type { vo } from "wailsjs/go/models";
 import Const from "./Const";
 
+const keys = [
+    {key1: "ship_stats", key2: "pr"},
+    {key1: "ship_stats", key2: "damage"},
+    {key1: "ship_stats", key2: "win_rate"},
+    {key1: "ship_stats", key2: "kd_rate"},
+    {key1: "overall_stats", key2: "damage"},
+    {key1: "overall_stats", key2: "win_rate"},
+    {key1: "overall_stats", key2: "kd_rate"},
+]
+
 export class AverageFactor {
     label: string
     friend: string
@@ -16,35 +26,21 @@ export class Average {
         this.battle = battle
     }
 
-    calc(excludePlayerID: number[]): AverageFactor[] {
-        const friends = this.battle.teams[0].players.filter((it) => !excludePlayerID.includes(it.player_info.id))
-        const enemies = this.battle.teams[1].players.filter((it) => !excludePlayerID.includes(it.player_info.id))
-
-        const result: AverageFactor[] = [];
-        [
-            {key1: "ship_stats", key2: "pr"},
-            {key1: "ship_stats", key2: "damage"},
-            {key1: "ship_stats", key2: "win_rate"},
-            {key1: "ship_stats", key2: "kd_rate"},
-            {key1: "overall_stats", key2: "damage"},
-            {key1: "overall_stats", key2: "win_rate"},
-            {key1: "overall_stats", key2: "kd_rate"},
-        ].forEach(it => {
-            result.push(calcFactor(it.key1, it.key2, friends, enemies))
-        });
-
-        return result
+    calc(excludePlayerIDs: number[]): AverageFactor[] {
+        const friends = this.battle.teams[0].players.filter((it) => !excludePlayerIDs.includes(it.player_info.id))
+        const enemies = this.battle.teams[1].players.filter((it) => !excludePlayerIDs.includes(it.player_info.id))
+        return keys.map((it) => calcFactor(it.key1, it.key2, friends, enemies))
     }
 }
 
 function calcFactor(key1: string, key2: string, friends: vo.Player[], enemies: vo.Player[]): AverageFactor {
-    const friend = friends.map((it) => it[key1][key2]).reduce((a, b) => a + b, 0) / friends.length || 0
-    const enemy = enemies.map((it) => it[key1][key2]).reduce((a, b) => a + b, 0) / enemies.length || 0
-    const diff = friend - enemy
+    const friendAvg = average(key1, key2, friends)
+    const enemyAvg = average(key1, key2, enemies)
+    const diff = friendAvg - enemyAvg
+
+    let sign = diff > 0 ? "+" : "";
     let colorClass = "";
-    let sign = "";
     if (diff > 0) {
-        sign = "+";
         colorClass = "higher";
     } else if (diff < 0) {
         colorClass = "lower";
@@ -52,9 +48,21 @@ function calcFactor(key1: string, key2: string, friends: vo.Player[], enemies: v
 
     return {
         label: Const.COLUMN_NAMES[key1].min + ":" + Const.COLUMN_NAMES[key2].min,
-        friend: friend.toFixed(Const.DIGITS[key2]),
-        enemy: enemy.toFixed(Const.DIGITS[key2]),
+        friend: friendAvg.toFixed(Const.DIGITS[key2]),
+        enemy: enemyAvg.toFixed(Const.DIGITS[key2]),
         diff: sign + diff.toFixed(Const.DIGITS[key2]),
         colorClass: colorClass,
     }
+}
+
+function average(key1: string, key2: string, players: vo.Player[]): number {
+    const values = players
+        .filter((it) => it[key1]["battles"] !== 0)
+        .map((it) => it[key1][key2] as number)
+
+    if (values.length === 0) {
+        return 0
+    }
+
+    return values.reduce((a, b) => a + b, 0) / values.length
 }
