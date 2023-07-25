801100326b332b980616f204d88b60f05a557bfe
tona0516 <tonango.0516@gmail.com>
2023-07-25 14:07:41 +0900

    Auto scroll for logging

--- frontend/src/other_component/Logging.svelte
+++ frontend/src/other_component/Logging.svelte
@@ -1,9 +1,21 @@
 <script lang="ts">
+  import { storedLogs } from "../stores";
+
   export let logs: string[];
+
+  const elementID = "logging";
+
+  storedLogs.subscribe(() => {
+    const loggingID = document.getElementById(elementID);
+    if (loggingID) {
+      // Note: subscribeが更新前にコールされるため更新後にスクロールさせるために少し遅延させる
+      setTimeout(() => (loggingID.scrollTop = loggingID.scrollHeight), 0.5);
+    }
+  });
 </script>
 
 <div class="center">
-  <div class="logging overflow-scroll bg-dark p-2 mx-2">
+  <div class="logging overflow-scroll bg-dark p-1 mx-1" id={elementID}>
     {#each logs as log}
       {log}
       <br />
