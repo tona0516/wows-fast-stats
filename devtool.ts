0d0c5aeec20b8384e396974a3aa7a25761a04de2
tona0516 <tonango.0516@gmail.com>
2023-11-10 09:13:17 +0900

    Migrate from mage to devtool.ts for task runner

---
+++ devtool.ts
@@ -0,0 +1,178 @@
+import { Command } from "commander";
+import fs from "node:fs";
+import path from "node:path";
+import archiver from "archiver";
+import shelljs from "shelljs";
+import readlineSync from "readline-sync";
+
+const APP_NAME = "wows-fast-stats";
+const SEMVER = "0.11.1";
+const BINARY_NAME = `${APP_NAME}-${SEMVER}.exe`;
+const FRONTEND_NPM_COMMAND = "npm --prefix ./frontend";
+
+namespace LDFlags {
+  const COMMON = {
+    "main.AppName": APP_NAME,
+    "main.Semver": SEMVER,
+  } as const;
+
+  export const DEV = {
+    ...COMMON,
+    "main.DiscordWebhookURL": fs
+      .readFileSync("discord_webhook_url_dev", "utf-8")
+      .trim(),
+    "main.IsDev": true,
+  };
+
+  export const PROD = {
+    ...COMMON,
+    "main.DiscordWebhookURL": fs
+      .readFileSync("discord_webhook_url_prod", "utf-8")
+      .trim(),
+  };
+}
+
+function getFormattedLDFlags(isDev: boolean): string {
+  const flags = isDev ? LDFlags.DEV : LDFlags.PROD;
+  return Object.entries(flags)
+    .map((it) => `-X ${it[0]}=${it[1]}`)
+    .join(" ");
+}
+
+function exec(command: string) {
+  shelljs.exec(command, { env: { ...process.env, FORCE_COLOR: "true" } });
+}
+
+function setup() {
+  const goPkgs = [
+    "github.com/wailsapp/wails/v2/cmd/wails@latest",
+    "github.com/golangci/golangci-lint/cmd/golangci-lint@latest",
+  ];
+  goPkgs.forEach((pkg) => {
+    exec(`go install ${pkg}`);
+  });
+  exec(`${FRONTEND_NPM_COMMAND} ci`);
+}
+
+function lint() {
+  exec("golangci-lint run");
+  exec(`${FRONTEND_NPM_COMMAND} run check`);
+  exec(`${FRONTEND_NPM_COMMAND} run lint`);
+}
+
+function fmt() {
+  exec("golangci-lint run --fix");
+  exec("go fmt");
+  exec(`${FRONTEND_NPM_COMMAND} run fmt`);
+}
+
+function test() {
+  exec("go test -cover ./...");
+  exec(`${FRONTEND_NPM_COMMAND} run test`);
+}
+
+function dev() {
+  exec(`wails dev -ldflags "${getFormattedLDFlags(true)}"`);
+}
+
+function devPut() {
+  const testDir = "./test_install_dir/replays/";
+  const tempArenaInfoName = "tempArenaInfo.json";
+
+  const files = fs
+    .readdirSync(testDir)
+    .filter(
+      (file) => file.startsWith(tempArenaInfoName) && file !== tempArenaInfoName
+    );
+  files.forEach((file, i) => {
+    console.log(i, file);
+  });
+
+  const index = readlineSync.question("index? > ");
+  fs.copyFileSync(
+    path.join(testDir, files[Number(index)]),
+    path.join(testDir, tempArenaInfoName)
+  );
+}
+
+function build() {
+  test();
+  exec(
+    `wails build -ldflags "${getFormattedLDFlags(
+      false
+    )}" -platform windows/amd64 -o ${BINARY_NAME} -trimpath`
+  );
+}
+
+async function pkg() {
+  build();
+
+  const tempDir = fs.mkdtempSync("");
+  try {
+    const dstPath = path.join(tempDir, APP_NAME);
+
+    fs.mkdirSync(dstPath);
+    fs.copyFileSync(
+      path.join("./build/bin/", BINARY_NAME),
+      path.join(dstPath, BINARY_NAME)
+    );
+
+    const archive = archiver("zip", {});
+    const output = fs.createWriteStream(`${APP_NAME}.zip`);
+    archive.pipe(output);
+
+    archive.directory(tempDir, false);
+
+    await archive.finalize();
+  } finally {
+    fs.rmSync(tempDir, { recursive: true, force: true });
+  }
+}
+
+function clean() {
+  ["config/", "cache/", "temp_arena_info/", "screenshot/"].forEach((target) => {
+    fs.rmSync(target, { recursive: true, force: true });
+  });
+}
+
+function main() {
+  const program = new Command();
+
+  program
+    .command("setup")
+    .description("Install dependencies for development.")
+    .action(setup);
+  program
+    .command("lint")
+    .description("Lint for golang and typescript codes.")
+    .action(lint);
+  program
+    .command("fmt")
+    .description("Format golang and typescript codes.")
+    .action(fmt);
+  program.command("test").description("Execute unit tests.").action(test);
+  program
+    .command("dev")
+    .description("Launch the application for developer mode.")
+    .action(dev);
+  program
+    .command("dev:put")
+    .description("Replace 'tempArenaInfo.json' in test install directory.")
+    .action(devPut);
+  program
+    .command("build")
+    .description("Make binary for Windows.")
+    .action(build);
+  program
+    .command("pkg")
+    .description("Make zip included the binary for Github Release.")
+    .action(pkg);
+  program
+    .command("clean")
+    .description("Remove directories and files created by the application.")
+    .action(clean);
+
+  program.parse();
+}
+
+main();
