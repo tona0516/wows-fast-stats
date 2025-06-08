74793cdaa5c683d8b1cf3ad1395a63d9ba7f7651
tona0516 <tona0516@gmail.com>
2025-06-09 00:45:43 +0900

    Update svelte 3 to 4

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
-import tsconfigPaths from 'vite-tsconfig-paths'
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    svelte(),
-    tsconfigPaths()],
+	plugins: [svelte(), tsconfigPaths()],
+	server: {
+		hmr: {
+			host: "localhost",
+			protocol: "ws",
+		},
+	},
 });
