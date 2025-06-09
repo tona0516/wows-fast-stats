e8f984980f469ba1f3f57f67622b0ed0e3d4dd44
tona0516 <tona0516@gmail.com>
2025-06-09 22:28:51 +0900

    Add daisyUI

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [svelte(), tsconfigPaths()],
+	plugins: [svelte(), tsconfigPaths(), tailwindcss()],
 	server: {
 		hmr: {
 			host: "localhost",
