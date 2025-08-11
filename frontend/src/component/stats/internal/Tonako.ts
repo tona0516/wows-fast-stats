1d0834a080ab030c1ee6cb38269af8d117078aad
tona0516 <tona0516@gmail.com>
2025-08-11 17:44:54 +0900

    Rename

--- frontend/src/component/stats/internal/Tonako.ts
+++ frontend/src/component/stats/internal/Tonako.ts
@@ -1,13 +1,13 @@
-import girlPointing from "src/assets/images/girl_pointing.png";
-import girlSorry from "src/assets/images/girl_sorry.png";
-import girlStandby from "src/assets/images/girl_standby.png";
+import tonakoPointing from "src/assets/images/tonako_pointing.png";
+import tonakoSorry from "src/assets/images/tonako_sorry.png";
+import tonakoStandby from "src/assets/images/tonako_standby.png";
 
 export class Tonako {
   private constructor(private readonly imgPath: string) {}
 
-  static readonly Pointing = new Tonako(girlPointing);
-  static readonly Standby = new Tonako(girlStandby);
-  static readonly Sorry = new Tonako(girlSorry);
+  static readonly Pointing = new Tonako(tonakoPointing);
+  static readonly Standby = new Tonako(tonakoStandby);
+  static readonly Sorry = new Tonako(tonakoSorry);
 
   getImgPath(): string {
     return this.imgPath;
