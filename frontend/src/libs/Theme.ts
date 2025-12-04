98786fff84c3d8e9c870e1a7491f020a7dcbd014
tona0516 <tona0516@gmail.com>
2025-12-04 21:42:28 +0900

    Remove theme

--- frontend/src/libs/Theme.ts
+++ frontend/src/libs/Theme.ts
@@ -1,19 +1,17 @@
 // Note: see style.css
-const LIGHTER_THEMES: string[] = ["light", "nord", "garden"] as const;
-const DARKER_THEMES: string[] = ["dark", "night", "black"] as const;
-const THEMES: string[] = [...LIGHTER_THEMES, ...DARKER_THEMES] as const;
-
 export namespace Theme {
+  const LIGHT = "light";
+  const DARK = "dark";
+
   export const isLighter = (): boolean => {
-    const current = getCurrent();
-    return LIGHTER_THEMES.includes(current);
+    return getCurrent() === LIGHT;
   };
 
   export const getCurrent = (): string => {
-    return localStorage.getItem("theme") || "light";
+    return localStorage.getItem("theme") || LIGHT;
   };
 
   export const getAll = (): string[] => {
-    return THEMES;
+    return [LIGHT, DARK];
   };
 }
