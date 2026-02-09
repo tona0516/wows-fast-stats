541d2a2dd2e3240aa902f152e57b3115cb389924
tona0516 <tona0516@gmail.com>
2026-02-10 00:06:39 +0900

    Fix msx damage column

--- frontend/src/components/tabledata/MaxDamageTableData.svelte
+++ frontend/src/components/tabledata/MaxDamageTableData.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
+  import ExternalLink from "@components/ExternalLink.svelte";
   import type { MaxDamageColumn } from "@libs/columns/MaxDamageColumn";
-    import { storedDisplayPref } from "@libs/stores";
+  import { storedDisplayPref } from "@libs/stores";
   import type { core } from "@wails/go/models";
 
   export let column: MaxDamageColumn;
@@ -10,11 +11,22 @@
 </script>
 
 {#if column.needsShow()}
-  <td class="{$storedDisplayPref.showBoarder ? "border border-gray-500" : ""} px-1 py-0.5" style="background-color: {column.getBgColorCode(player)}">
-    <div class="text-right">
-      <div class="tooltip" data-tip={param.shipInfo?.name}>
-        {param.damage}
-      </div>
+  <td
+    class="{$storedDisplayPref.showBoarder
+      ? 'border border-gray-500'
+      : ''} px-1 py-0.5"
+    style="background-color: {column.getBgColorCode(player)}"
+  >
+    <div class="flex flex-row">
+      <div>{param.damage}</div>
+      {#if param.shipInfo}
+        <div>
+          <span>, </span>
+          <ExternalLink url={param.shipInfo.url}>
+            {param.shipInfo.name}
+          </ExternalLink>
+        </div>
+      {/if}
     </div>
   </td>
 {/if}
